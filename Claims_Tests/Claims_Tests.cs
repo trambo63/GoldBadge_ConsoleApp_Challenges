576419601e08a25715af28299d3fea4a4d4495d1
Tom <tomrambo95@yahoo.com>
2020-09-29 15:22:14 -0400

    added tests for repo

--- Claims_Tests/Claims_Tests.cs
+++ Claims_Tests/Claims_Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using _02_Claims;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Claims_Tests
@@ -7,8 +9,32 @@ namespace Claims_Tests
     public class Claims_Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddClaimAddsClaim()
         {
+            Claim claim = new Claim();
+            ClaimRepo claimRepo = new ClaimRepo();
+            bool result = claimRepo.AddClaim(claim);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetClaimsGetsAllClaims()
+        {
+            Claim claim = new Claim();
+            ClaimRepo claimRepo = new ClaimRepo();
+            claimRepo.AddClaim(claim);
+            List<Claim> claimList = claimRepo.GetClaims();
+            bool listNotEmpty = claimList.Contains(claim);
+            Assert.IsTrue(listNotEmpty);
+        }
+        [TestMethod]
+        public void HandleClaimRemovesClaimFromDB()
+        {
+            Claim claim = new Claim();
+            ClaimRepo claimRepo = new ClaimRepo();
+            claimRepo.AddClaim(claim);
+            bool isHandled = claimRepo.HandleClaim(claim);
+            Assert.IsTrue(isHandled);
         }
     }
 }

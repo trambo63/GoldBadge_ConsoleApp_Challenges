80e0dd675c95426ea128857e55b3c18f18769c24
Tom <tomrambo95@yahoo.com>
2020-10-09 12:45:25 -0400

    Greeting complete

--- 05_Greeting/CustomerRepo.cs
+++ 05_Greeting/CustomerRepo.cs
@@ -23,6 +23,42 @@ namespace _05_Greeting
             return _customerDB;
         }
 
+        public Customer GetCustomerByName(string fistName)
+        {
+            foreach (Customer customer in _customerDB)
+            {
+                if (customer.FirstName.ToLower() == fistName.ToLower())
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+        public bool EditCustomer(string originalFirstName, Customer newCustomer)
+        {
+            Customer oldCustomer = GetCustomerByName(originalFirstName);
+
+            if (oldCustomer != null)
+            {
+                oldCustomer.FirstName = newCustomer.FirstName;
+                oldCustomer.LastName = newCustomer.LastName;
+                oldCustomer.CustomerType = newCustomer.CustomerType;
+                oldCustomer.Email = newCustomer.Email;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(Customer customer)
+        {
+            bool deleteResult = _customerDB.Remove(customer);
+            return deleteResult;
+        }
 
     }
 }
--- 05_Greeting/ProgramUI.cs
+++ 05_Greeting/ProgramUI.cs
@@ -38,10 +38,10 @@ namespace _05_Greeting
                         AddNewEmail();
                         break;
                     case "3":
-                        //EditEmail();
+                        EditEmail();
                         break;
                     case "4":
-                        //DeleteEmail();
+                        DeleteEmail();
                         break;
                     case "5":
                         keepThinking = false;
@@ -107,6 +107,103 @@ namespace _05_Greeting
                 keepThinking = false;
             }
             _customerRepo.AddEmail(myCustomer);
+            Console.WriteLine("Press any key to continue.........");
+            Console.ReadKey();
+        }
+
+        private void EditEmail()
+        {
+            Console.Clear();
+            Console.WriteLine("What Customer do you want to edit?");
+            Customer myCustomer = new Customer();
+            List<Customer> customerList = _customerRepo.GetAllEmails();
+            int count = 0;
+            foreach (var customer in customerList)
+            {
+                count++;
+                Console.WriteLine($"{count}: {customer.FirstName} {customer.LastName}");
+            }
+            int targetCostumer = int.Parse(Console.ReadLine());
+            int correctIndex = targetCostumer - 1;
+            if (correctIndex >= 0 && correctIndex < customerList.Count)
+            {
+                Customer desiredCustomer = customerList[correctIndex];
+                if (_customerRepo.EditCustomer(desiredCustomer.FirstName, myCustomer))
+                {
+                    Console.WriteLine($"{desiredCustomer.FirstName}");
+                    Console.WriteLine("Re-Enter First Name: ");
+                    myCustomer.FirstName = Console.ReadLine();
+                    Console.WriteLine($"{desiredCustomer.LastName}");
+                    Console.WriteLine("Re-Enter Last Name: ");
+                    myCustomer.LastName = Console.ReadLine();
+                    Console.WriteLine($"Type: {desiredCustomer.CustomerType}");
+                    bool keepThinking = true;
+                    while (keepThinking)
+                    {
+                        Console.WriteLine("Re-Enter Customer Type.....\n" +
+                            "1. Current \n" +
+                            "2. Past \n" +
+                            "3. Potential");
+                        string userResponse = Console.ReadLine();
+                        switch (userResponse)
+                        {
+                            case "1":
+                                myCustomer.CustomerType = CustomerType.Current;
+                                myCustomer.Email = myCustomer.CurrentCustomerEmail();
+                                break;
+                            case "2":
+                                myCustomer.CustomerType = CustomerType.Past;
+                                myCustomer.Email = myCustomer.PastCustomerEmail();
+                                break;
+                            case "3":
+                                myCustomer.CustomerType = CustomerType.Potential;
+                                myCustomer.Email = myCustomer.PotentialCustomerEmail();
+                                break;
+                            default:
+                                Console.WriteLine("Invalid");
+                                Console.ReadKey();
+                                break;
+                        }
+                        keepThinking = false;
+                    }
+                    _customerRepo.DeleteCustomer(desiredCustomer);
+                    _customerRepo.AddEmail(myCustomer);
+                    Console.WriteLine("Press any key to continue.........");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private void DeleteEmail()
+        {
+            Console.Clear();
+            Console.WriteLine("What Customer do you want to Delete?");
+            Customer myCustomer = new Customer();
+            List<Customer> customerList = _customerRepo.GetAllEmails();
+            int count = 0;
+            foreach (var customer in customerList)
+            {
+                count++;
+                Console.WriteLine($"{count}: {customer.FirstName} {customer.LastName}");
+            }
+            int targetCostumer = int.Parse(Console.ReadLine());
+            int correctIndex = targetCostumer - 1;
+            if (correctIndex >= 0 && correctIndex < customerList.Count)
+            {
+                Customer desiredCustomer = customerList[correctIndex];
+                if (_customerRepo.DeleteCustomer(desiredCustomer))
+                {
+                    Console.WriteLine($"{desiredCustomer} has been Deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Ivalid");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ivalid");
+            }
             Console.WriteLine("Press any key to continue.........");
             Console.ReadKey();
         }
